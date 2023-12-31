35fcf2b91928dc3f8b8d288bdc119dc8ab35b531
Abdur Rohit <sheikhabdurrohit@gmail.com>
2023-12-31 18:51:01 +0530

    rem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GroupExample from './utils/cards';
-import {switchToHederaNetworkethers} from './utils/metamaskService';
+import {switchToHederaNetworkethers} from './contract/metamaskService';
 import { ethers } from "ethers";
 import ModalForm from './utils/addNewModal';
 
 
-function viewModalform(){
-  return(
-    <>
-      <ModalForm></ModalForm>
-    </>
-  )
-}
 
 function App() {
 
--- src/contract/hederaService.js
+++ src/contract/hederaService.js
@@ -9,7 +9,7 @@ export const sendHbar = async (client, fromAddress, toAddress, amount, operatorP
   const transferHbarTransactionSigned = await transferHbarTransaction.sign(operatorPrivateKey);
   const transferHbarTransactionResponse = await transferHbarTransactionSigned.execute(client);
 
-  // Get the child receipt or child record to return the Hedera Account ID for the new account that was created
+   
   const transactionReceipt = await new TransactionReceiptQuery()
     .setTransactionId(transferHbarTransactionResponse.transactionId)
     .setIncludeChildren(true)
